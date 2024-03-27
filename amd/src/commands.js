71797fc91a59a59dedf517182c7697f960558c0e
Aniket_Kr <aniketk9211@gmail.com>
2024-03-27 23:26:52 +0530

    modify the model body

--- amd/src/commands.js
+++ amd/src/commands.js
@@ -1,117 +1,61 @@
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
-/**
- * Commands helper for the Moodle tiny_moldraw plugin.
- *
- * @module      tiny_moldraw/commands
- * @copyright   2024 Venkatesan Rangarajan <venkatesanrpu@gmail.com>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 import { getButtonImage } from "editor_tiny/utils";
 import { get_string as getString } from "core/str";
-import Templates from "core/templates";
 import Modal from "core/modal";
-import Config from "core/config";
-import {
-  component,
-  startMolDrawButtonName,
-  startMolDrawMenuItemName,
-  icon,
-} from "./common";
-
-/**
- * Handle the action for your plugin.
- * @param {TinyMCE.editor} editor The tinyMCE editor instance.
- */
+import { component, startMolDrawButtonName, startMolDrawMenuItemName, icon } from "./common";
+import { loadBody, loadFooter } from "./chem/body";
 
 const handleAction = async (editor) => {
-  await Modal.create({
-    title: await getString("sketchtitle", "tiny_moldraw"),
-    body: `
-        ${await Templates.render("tiny_moldraw/moldraw_iframe", {
-      src: `${Config.wwwroot}/lib/editor/tiny/plugins/moldraw/chem/chem.html`,
-    })}
-        
-      `,
-    footer: `<p>Example body content</p>
-      <button id="actionbutton" class="actionbutton">Insert</button>
-      <script src="http://localhost/lib/editor/tiny/plugins/moldraw/chem/chem2.js"></script>`,
-    show: true,
-    removeOnClose: true,
-  });
+  try {
+    const title = await getString("sketchtitle", "tiny_moldraw");
+    const bodyContent = await loadBody();
+    const footerContent = await loadFooter();
 
-  // -------------------------
-  // Calculate the width and height for the modal
-  let modalWidth = 850;
-  let modalHeight = 600; // 80% of the screen height
-  let screenHeight = window.innerHeight;
-  let topPosition = (screenHeight - modalHeight) / 2;
-  // Apply CSS styles to the modal elements
-  document.querySelector(".modal-dialog").style.cssText =
-    "max-width: unset; width: " +
-    modalWidth +
-    "px; height: " +
-    modalHeight +
-    "px; margin: " +
-    topPosition +
-    "px auto; padding: 0;";
-  document.querySelector(".modal-content").style.cssText =
-    "max-height: 600px; height: 100vh;";
-  document.querySelector(".modal-body").style.cssText = "padding: 0;";
-  window.console.log(editor);
+    await Modal.create({
+      title: title,
+      body: bodyContent,
+      footer: footerContent,
+      show: true,
+      removeOnClose: true,
+    });
 
+    const modalWidth = 850;
+    const modalHeight = 600;
+    const screenHeight = window.innerHeight;
+    const topPosition = (screenHeight - modalHeight) / 2;
+    document.querySelector(".modal-dialog").style.cssText =
+      `max-width: unset; width: ${modalWidth}px; height: ${modalHeight}px; margin: ${topPosition}px auto; padding: 0;`;
+    document.querySelector(".modal-content").style.cssText =
+      "max-height: 600px; height: 100vh;";
+    document.querySelector(".modal-body").style.cssText = "padding: 0;";
+  } catch (error) {
+    console.error("Error handling action:", error);
+  }
 };
 
-/**
- * Get the setup function for the buttons.
- *
- * This is performed in an async function which ultimately returns the registration function as the
- * Tiny.AddOnManager.Add() function does not support async functions.
- *
- * @returns {function} The registration function to call within the Plugin.add function.
- */
 export const getSetup = async () => {
-  const [
-    startMolDrawButtonNameTitle,
-    startMolDrawMenuItemNameTitle,
-    buttonImage,
-  ] = await Promise.all([
-    getString("button_startMolDraw", component),
-    getString("menuitem_startMolDraw", component),
-    getButtonImage("icon", component),
-  ]);
+  try {
+    const [startMolDrawButtonNameTitle, startMolDrawMenuItemNameTitle, buttonImage] = await Promise.all([
+      getString("button_startMolDraw", component),
+      getString("menuitem_startMolDraw", component),
+      getButtonImage("icon", component),
+    ]);
 
-  return (editor) => {
-    // Register the Moodle SVG as an icon suitable for use as a TinyMCE toolbar button.
-    editor.ui.registry.addIcon(icon, buttonImage.html);
+    return (editor) => {
+      editor.ui.registry.addIcon(icon, buttonImage.html);
 
-    // Register the startMolDraw Toolbar Button.
-    editor.ui.registry.addButton(startMolDrawButtonName, {
-      icon,
-      tooltip: startMolDrawButtonNameTitle,
-      onAction: () => handleAction(editor),
-    });
+      editor.ui.registry.addButton(startMolDrawButtonName, {
+        icon,
+        tooltip: startMolDrawButtonNameTitle,
+        onAction: () => handleAction(editor),
+      });
 
-    // Add the startMolDraw Menu Item.
-    // This allows it to be added to a standard menu, or a context menu.
-    editor.ui.registry.addMenuItem(startMolDrawMenuItemName, {
-      icon,
-      text: startMolDrawMenuItemNameTitle,
-      onAction: () => handleAction(editor),
-    });
-  };
+      editor.ui.registry.addMenuItem(startMolDrawMenuItemName, {
+        icon,
+        text: startMolDrawMenuItemNameTitle,
+        onAction: () => handleAction(editor),
+      });
+    };
+  } catch (error) {
+    console.error("Error setting up plugin:", error);
+  }
 };
---
+++ chem/body.js
@@ -0,0 +1,55 @@
+export function loadBody() {
+  return new Promise((resolve, reject) => {
+      try {
+          const bodyContent = `<div class="wrapper" style="text-align: center">
+
+              <div class="drawing_canvas-container">
+              <canvas id="sketcher"></canvas><br><br>
+              </div>
+
+              <div class="inputs_size">
+              <div class="input_width">
+                  <label id="label_width_input_molstructure" for="width_input_molstructure">Width: </label>
+                  <input type="number" id="width_input_molstructure" name="width" max="500" min="10" step="5" placeholder="100">
+                  <br><br>
+              </div>
+
+              <div class="input_height">
+                  <label id="label_height_input_molstructure" for="height_input_molstructure">Height: </label>
+                  <input type="number" id="height_input_molstructure" name="height" max="500" min="10" step="5"
+                  placeholder="100"><br><br>
+              </div>
+
+              </div>
+
+              <div class="button_size">
+              <button id="button-size-button" onclick="function_resize()">Resize</button><br><br>
+              </div>
+
+              <div class="viewer_canvas-container">
+              <canvas id="sketcher-viewer-atto"></canvas>
+              </div>
+
+          </div>
+          <button id=" insert" onclick="insert()">Insert</button>
+          <script src="http://localhost/lib/editor/tiny/plugins/moldraw/chem/app.js"></script>`;
+          
+          resolve(bodyContent);
+      } catch (error) {
+          reject(error);
+      }
+  });
+}
+
+export function loadFooter() {
+  return new Promise((resolve, reject) => {
+      try {
+          const footerContent = `<p>Example body content</p>
+          <button id="actionbutton" class="actionbutton">Insert</button>`;
+          
+          resolve(footerContent);
+      } catch (error) {
+          reject(error);
+      }
+  });
+}
