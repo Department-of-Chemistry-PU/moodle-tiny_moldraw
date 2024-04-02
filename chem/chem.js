c4d5502b64fd56662ebd858ca4e154fcf8927b17
Aniket_Kr <aniketk9211@gmail.com>
2024-04-02 16:56:18 +0530

    resize Working

--- chem/chem.js
+++ chem/chem.js
@@ -1,54 +1,51 @@
-$(document).ready(function () {
-  $("#actionbutton").click(handleClick);
+document.addEventListener("DOMContentLoaded", function () {
+  var actionButton = document.getElementById("actionbutton");
+  actionButton.addEventListener("click", closeModal);
 
-  isPreview = true;
-  function handleClick() {
-    let mol = sketcher.getMolecule();
-    if (isPreview) {
-      if (mol === undefined) {
-        alert("Canvas is Blank");
-      } else {
-        console.log("if part run");
-        $(this).attr("data-state", "preview");
-        outputImage();
-        $(this).html("Confirm & Close").attr("data-state", "confirm");
-        isPreview = false;
-      }
-    } else {
-      console.log("else part run");
-      closeModal();
-    }
-  }
-  function handleClick() {
-    closeModal();
-  }
+  // var isPreview = true;
 
-  function outputImage() {
-    let mol = sketcher.getMolecule();
+  // function handleClick() {
+  //   var mol = view_canvas.getMolecule();
+  //   if (isPreview) {
+  //     if (mol === undefined) {
+  //       alert("Canvas is Blank");
+  //     } else {
+  //       console.log("if part run");
+  //       this.setAttribute("data-state", "preview");
+  //       outputImage();
+  //       this.innerHTML = "Confirm & Close";
+  //       this.setAttribute("data-state", "confirm");
+  //       isPreview = false;
+  //     }
+  //   } else {
+  //     console.log("else part run");
+  //     closeModal();
+  //   }
+  // }
 
-    let molFile = ChemDoodle.writeMOL(mol);
-      console.log(molFile);
-    src = ChemDoodle.io.png.string(sketcher);
-
-    let img = document.createElement("img");
-    img.src = src;
-    img.width = prompt("Enter Width: ");
-    img.height = prompt("Enter Height: ");
-    var outputDiv = document.getElementById("output");
-    outputDiv.appendChild(img);
-  }
+  // function outputImage() {
+  //   var mol = sketcher.getMolecule();
+  //   var molFile = ChemDoodle.writeMOL(mol);
+  //   console.log(molFile);
+  //   var src = ChemDoodle.io.png.string(sketcher);
+  //   var img = document.createElement("img");
+  //   img.src = src;
+  //   img.width = prompt("Enter Width: ");
+  //   img.height = prompt("Enter Height: ");
+  //   var outputDiv = document.getElementById("output");
+  //   outputDiv.appendChild(img);
+  // }
 
   function closeModal() {
-    if (window.parent.tinyMCE && window.parent.tinyMCE.activeEditor) {
-      let mol = sketcher.getMolecule();
-      let src = ChemDoodle.io.png.string(sketcher);
-      let molFile = ChemDoodle.writeMOL(mol);
-
-      // let width = prompt("width:");
-      // let height = prompt("height:");
-      let width = 400;
-      let height = 300;
-
+    if (
+      window.parent.tinyMCE &&
+      window.parent.tinyMCE.activeEditor
+    ) {
+      var mol = sketcher_viewer.getMolecule();
+      var src = ChemDoodle.io.png.string(sketcher_viewer);
+      var molFile = ChemDoodle.writeMOL(mol);
+      var width = 400;
+      var height = 300;
       var content =
         '<img src="' +
         src +
@@ -57,7 +54,6 @@ $(document).ready(function () {
         'px" height="' +
         height +
         'px">';
-
       window.parent.tinyMCE.activeEditor.execCommand(
         "mceInsertContent",
         0,
@@ -68,8 +64,10 @@ $(document).ready(function () {
         0,
         "<!--" + molFile + "-->"
       );
-
-      $(window.parent.document).find(".modal").find(".close").click();
+      window.parent.document
+        .querySelector(".modal")
+        .querySelector(".close")
+        .click();
     }
   }
 });
